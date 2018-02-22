a36138d1679749ea3538b23bde3648313c5ea30d
GeeorgeUK <geeorge.uk@gmail.com>
2018-02-22 16:46:57 +0000

    Update main.lua

--- lua/main.lua
+++ lua/main.lua
@@ -1,1 +1,23 @@
 -- main.lua
+
+plugin.onEnable(
+  function()
+    logger.info("###############################")
+    logger.info(" Welcome to Titanium 'alpha' ")
+    logger.info("")
+    logger.info(" Version: alpha_0")
+    logger.info(" Author: CuddlesTech")
+    logger.info("###############################")
+  end
+)
+
+plugin.onDisable(
+  function()
+    logger.warn("###############################")
+    logger.warn(" TITANIUM HAS BEEN DISABLED ")
+    logger.warn("")
+    logger.warn(" You should enable Titanium ")
+    logger.warn(" to protect player builds!")
+    logger.warn("###############################")
+  end
+)
